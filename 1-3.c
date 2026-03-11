aa59ace605c3a3173ee93caec46f959913b4f47e
HELLOOOOW <201902669@o.cnu.ac.kr>
2026-03-11 15:13:17 +0900

    fix templete

--- 1-3.c
+++ 1-3.c
@@ -2,12 +2,7 @@
 
 int main()
 {
-    int age;
-    char grade;
-    float gpa;
-    
-    scanf("");
-    printf("%d %c %f\n", age, grade, gpa);
+
 
     return 0;
 }
--- 2-1.c
+++ 2-1.c
@@ -2,14 +2,6 @@
 
 int main()
 {
-    
-    printf("char: %u\n", 0);
-    printf("short: %u\n", 0);
-    printf("int: %u\n", 0);
-    printf("unsigned int: %u\n", 0);
-    printf("long: %u\n", 0);
-    printf("long long: %u\n", 0);
-    printf("float: %u\n", 0);
 
     return 0;
 }
