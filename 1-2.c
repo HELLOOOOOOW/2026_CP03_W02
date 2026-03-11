0c5fb2e231e2112ad8aa2a27321699b2f0762808
HELLOOOOOOW <127479879+HELLOOOOOOW@users.noreply.github.com>
2026-03-11 13:21:20 +0900

    Add files via upload

---
+++ 1-2.c
@@ -0,0 +1,11 @@
+int main()
+{
+    printf("%s %s %s\n", "name", "id", "value");
+    printf("%s %d %f\n", "kim", 12, 4.12);
+    printf("%s %d %f\n", "lee", 1922, 3.21);
+    printf("%s %d %f\n", "park", 432, 1.0);
+    
+    
+
+    return 0;
+}
---
+++ 2-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("char: %u\n", 0);
+    printf("short: %u\n", 0);
+    printf("int: %u\n", 0);
+    printf("unsigned int: %u\n", 0);
+    printf("long: %u\n", 0);
+    printf("long long: %u\n", 0);
+    printf("float: %u\n", 0);
+
+    return 0;
+}
